837618ad487e8889a5abdcd7440568ce7dc40624
yenadaa <rlaekdpsk33@naver.com>
2026-07-23 19:29:32 +0900

    입력 기능_1_1

---
+++ src/InputView.js
@@ -0,0 +1,11 @@
+import { Console } from '@woowacourse/mission-utils';
+
+class InputClass {
+
+    async input() { //클래스 안에서는 function 빼야힘
+        const inputText =  await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+        return inputText;
+    }
+}
+
+
